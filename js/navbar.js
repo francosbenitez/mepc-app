2bba8de358fc612b6c1033e9b3888618bed3ff28
francosbenitez <francosbenitez@gmail.com>
2021-04-17 23:39:47 -0300

    update & upload examples!

--- js/navbar.js
+++ js/navbar.js
@@ -1,7 +1,27 @@
-(function() {
-    'use strict'
+const cardTransform = () => {
+    const serviceCard = document.querySelector('#services .service-summary .card');
+    const moreInfo = document.querySelectorAll('#services .service-summary .card .moreInfo');
+    const cardImg = document.querySelectorAll('#services .service-summary .card img')
 
-    document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function() {
-        document.querySelector('.offcanvas-collapse').classList.toggle('open')
-    })
-})()
+    // serviceCard.addEventListener('mouseover', () => {
+        
+    // });
+}
+
+cardTransform();
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Burger Animate
+        burger.classList.toggle('toggle');
+    });
+    
+}
+
+navSlide();
