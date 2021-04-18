441738ac1e6cc122012cae8ceff8bdb2d08df4b2
francosbenitez <francosbenitez@gmail.com>
2021-04-18 11:17:43 -0300

    add collapse button!

--- js/navbar.js
+++ js/navbar.js
@@ -1,27 +1,11 @@
-const cardTransform = () => {
-    const serviceCard = document.querySelector('#services .service-summary .card');
-    const moreInfo = document.querySelectorAll('#services .service-summary .card .moreInfo');
-    const cardImg = document.querySelectorAll('#services .service-summary .card img')
-
-    // serviceCard.addEventListener('mouseover', () => {
-        
-    // });
-}
-
-cardTransform();
-
 const navSlide = () => {
     const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
+    const nav = document.querySelector('.nav-ul');
 
     burger.addEventListener('click', () => {
-        // Toggle Nav
         nav.classList.toggle('nav-active');
-
-        // Burger Animate
         burger.classList.toggle('toggle');
     });
-    
 }
 
 navSlide();
