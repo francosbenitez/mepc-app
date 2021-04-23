49165b5058e079eaff3dbda89fd8db2a16369d34
francosbenitez <francosbenitez@gmail.com>
2021-04-23 09:57:56 -0300

    improving js!

--- js/navbar-2.js
+++ js/navbar-2.js
@@ -9,3 +9,13 @@ const navSlide = () => {
 }
 
 navSlide();
+
+let header = document.getElementById("nav");
+let links = header.getElementsByClassName("link");
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
