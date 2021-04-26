26e65ff315df3859974b83b2a5245bc66e7cfd14
francosbenitez <francosbenitez@gmail.com>
2021-04-26 19:05:54 -0300

    fix javascript code for navbar!

--- js/navbar.js
+++ js/navbar.js
@@ -10,12 +10,13 @@ const navSlide = () => {
 
 navSlide();
 
-let header = document.getElementById("nav");
-let links = header.getElementsByClassName("link");
-for (let i = 0; i < links.length; i++) {
-  links[i].addEventListener("click", function () {
-    let current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
-}
+(function () {
+  var current = location.pathname.split('/')[1];
+  if (current === "") return;
+  var menuItems = document.querySelectorAll('.nav-ul a');
+  for (var i = 0, len = menuItems.length; i < len; i++) {
+      if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+          menuItems[i].className += "is-active";
+      }
+  }
+})();
