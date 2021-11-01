6471c7b3686d937ce2902876d64280b40a02820b
francosbenitez <francosbenitez@gmail.com>
2021-10-31 21:27:11 -0300

    chore: remove bootstrap-vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    // 'bootstrap-vue/nuxt',
     '@nuxt/content'
   ],
 
