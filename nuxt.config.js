1e05bda005443a4aee385771a31fc69a9f377d5c
francosbenitez <francosbenitez@gmail.com>
2021-11-01 11:06:55 -0300

    chore: updated nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,8 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mepc',
+    title: 'Home',
+    titleTemplate: '%s | Movimiento Estudiantil por la Psicología Científica (MEPC)',
     htmlAttrs: {
       lang: 'en'
     },
