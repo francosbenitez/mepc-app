509d5137d602e6d208e8cdead3690d797011eb57
francosbenitez <francosbenitez@gmail.com>
2021-11-03 10:56:21 -0300

    feat: created footer mobile version

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,17 +1,32 @@
 <template>
-    <footer>
+    <footer class="footer">
         <div class="footer__icons">
-            Encuéntranos en 
+            <div class="footer__icons-text">
+                Encuéntranos en 
+            </div>
+            <div class="footer__icons-icons">
+                <TwitterIcon />
+                <InstagramIcon />
+                <YoutubeIcon />
+                <DiscordIcon />
+                <LinkedinIcon />
+            </div>
         </div>
-
+        <img src="~assets/svg/logo.svg" alt="" class="footer__logo">
         <div class="footer__links">
-            <img src="~assets/svg/logo.svg" alt="" class="footer__logo">
             <a href="#" target="_blank" rel="noopener">
-                
+                Grupos de estudio
+            </a>
+            <a href="#" target="_blank" rel="noopener">
+                Documentos
+            </a>
+            <a href="#" target="_blank" rel="noopener">
+                Preguntas frecuentes
             </a>
         </div>
-
-        <EmailIcon />
+        <div class="footer__copyright"> 
+            &copy; MEPC {{ new Date().getFullYear() }}
+        </div>
         
         <!-- <p>MEPC © 2021</p>
         <p>Desarrollado con ☕️ y <a href="https://github.com/francosbenitez/mepc" target="_blank" rel="noopener">
@@ -27,23 +42,44 @@
 </template>
 
 <script>
-import EmailIcon from 'icons/EmailVariant'
 import TwitterIcon from 'icons/Twitter'
-import GithubIcon from 'icons/Github'
-import DevpostIcon from 'icons/HexagonOutline'
+import InstagramIcon from 'icons/Instagram'
+import DiscordIcon from 'icons/Discord'
+import YoutubeIcon from 'icons/Youtube'
 import LinkedinIcon from 'icons/Linkedin'
+
 export default {
   components: {
-    EmailIcon,
     TwitterIcon,
-    GithubIcon,
-    DevpostIcon,
+    InstagramIcon,
+    DiscordIcon,
+    YoutubeIcon,
     LinkedinIcon
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.footer {
+    outline: dashed 1px black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+
+.footer__icons-icons {
+    color: $color__primary;
+}
+
+.footer__links {
+    display: grid;
+    grid-column-gap: 1.25rem;
+    grid-row-gap: 0.5rem;
+    column-gap: 1.25rem;
+    row-gap: 0.5rem;
+}
+
 .footer__logo {
     height: 3rem;
 }
