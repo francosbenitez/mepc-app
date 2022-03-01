edcb161538d301a9164238b13b6fce474fefc6d5
francobenitez <franco.benitez@hogarthww.com>
2022-03-01 00:23:31 -0300

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,8 +44,8 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/style-resources',
-    // '@nuxtjs/eslint-module',
-    // '@nuxtjs/stylelint-module',
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
     '@nuxtjs/google-fonts'
   ],
 
