1420bba1eab34b5335ace0bee2a58942849c398e
francobenitez <franco.benitez@hogarthww.com>
2022-05-17 13:27:40 -0300

    feat: add NuxtLink

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -2,9 +2,11 @@
   <header class="main-header">
     <nav class="navbar">
       <ul class="nav-ul">
-        <li><a href="./que-estudiamos.html">¿Qué estudiamos?</a></li>
-        <li><a href="./documentos.html">Documentos</a></li>
-        <li><a href="./recursos.html">Recursos</a></li>
+        <li>
+          <NuxtLink to="/what-do-we-study">¿Qué estudiamos?</NuxtLink>
+        </li>
+        <li><NuxtLink to="/documents">Documentos</NuxtLink></li>
+        <li><NuxtLink to="/resources">Recursos</NuxtLink></li>
       </ul>
       <div class="nav-btn">
         <div class="line-1"></div>
