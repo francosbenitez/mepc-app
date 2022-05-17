cd0c7cd9cd5bbe963cbc3f545ac21014ab7d8ba6
francobenitez <franco.benitez@hogarthww.com>
2022-05-17 13:06:57 -0300

    fix: fix scss gradient issue

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'prettier'],
+  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
   rules: {},
