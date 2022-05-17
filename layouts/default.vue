f2e742bf4c1db9c086757da1bb69b2ebefb36a50
francobenitez <franco.benitez@hogarthww.com>
2022-05-17 12:21:38 -0300

    feat: add layouts | chore: remove recommended eslint

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <TheNavbar />
+    <Nuxt />
+    <!-- <TheFooter /> -->
+  </div>
+</template>
+
+<script>
+import TheNavbar from '@/components/TheNavbar'
+
+export default {
+  components: {
+    TheNavbar,
+  },
+}
+</script>
