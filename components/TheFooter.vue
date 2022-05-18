74c12f7af0baae7a6f11a2cc432e63d474f676a6
francobenitez <franco.benitez@hogarthww.com>
2022-05-18 01:57:23 -0300

    style: improve footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,41 +1,62 @@
 <template>
-  <footer class="site-footer">
-    <p>MEPC © 2021</p>
-    <p>
-      Desarrollado con ☕️ y
-      <a
-        href="https://github.com/francosbenitez/mepc"
-        target="_blank"
-        rel="noopener"
-      >
-        <i class="fas fa-code-branch"></i
-      ></a>
-      por
-      <a href="https://francosbenitez.github.io" target="_blank" rel="noopener">
-        Franco Sebastián Benítez</a
-      >
-      <span class="float-right" aria-hidden="true">
-        <a href="#" class="back-to-top">
-          <i class="fas fa-chevron-up fa-2x"></i>
-        </a>
-      </span>
-    </p>
+  <footer class="footer">
+    <div class="footer__content">
+      <p>MEPC © 2021</p>
+      <p>
+        Desarrollado con ☕️ y
+        <a
+          href="https://github.com/francosbenitez/mepc"
+          target="_blank"
+          rel="noopener"
+        >
+          <GitHub
+        /></a>
+        por
+        <a
+          href="https://www.francosbenitez.com/"
+          target="_blank"
+          rel="noopener"
+        >
+          Franco Sebastián Benítez</a
+        >
+      </p>
+    </div>
   </footer>
 </template>
 
+<script>
+import GitHub from './assets/icons/github.svg?inline'
+
+export default {
+  components: {
+    GitHub,
+  },
+}
+</script>
+
 <style lang="scss">
-.site-footer {
-  color: var(--color-text);
+.footer {
+  color: $color__text;
   text-align: center;
   font-size: 13px;
-  max-width: 1200px;
-}
+  /* max-width: 1200px; */
+  padding: 0.75rem 0 1rem;
+  box-shadow: 0 0 2rem -1rem rgba(0, 0, 0, 0.2);
 
-.site-footer p {
-  margin: 10px 0 10px 0;
-}
+  .footer__content {
+    margin: 0 auto;
+    max-width: 1152px;
+    position: relative;
+    width: 90%;
 
-.site-footer .float-right {
-  float: right;
+    p {
+      margin: 10px 0 10px 0;
+    }
+
+    svg {
+      width: 10px;
+      height: 10px;
+    }
+  }
 }
 </style>
