2a9d2cd16bf99622d6222291e86a53df10729a32
francobenitez <franco.benitez@hogarthww.com>
2022-05-18 12:28:19 -0300

    feat: reorganize default.scss & develop HomeFeatures component

--- components/HomeFeatures2.vue
+++ components/HomeFeatures2.vue
@@ -1,12 +1,80 @@
 <template>
-  <div></div>
+  <section id="collaboration">
+    <div class="titles">
+      <h1>¿Cómo colaborar?</h1>
+      <hr />
+    </div>
+    <section class="collaboration__content">
+      <div class="container-feature">
+        <div class="avatar">
+          <div class="avatar-image">
+            <img src="./assets/images/speaker.svg" alt="Avatar MEPC" />
+          </div>
+        </div>
+      </div>
+      <div class="container-description">
+        <div class="asd">
+          <h2 class="feature-title">Corré la voz</h2>
+          <p class="feature-description">
+            Una mejor educación es para todes. Ayudanos a correr la voz
+            siguiéndonos y compartiendo nuestros contenidos en
+            <a href="https://twitter.com/MEPC_oficial">Twitter</a>,
+            <a href="https://www.youtube.com/channel/UCJpN1ANkID-67MQ2alMek-g"
+              >YouTube</a
+            >, <a href="https://www.facebook.com/mepc.oficial/">Facebook</a>,
+            <a href="https://www.instagram.com/mepc.oficial/">Instagram</a> y
+            otras redes sociales
+          </p>
+          .
+        </div>
+      </div>
+    </section>
+    <section class="collaboration__content">
+      <div class="container-feature">
+        <div class="avatar">
+          <div class="avatar-image">
+            <img src="./assets/images/remote-team.svg" alt="Avatar MEPC" />
+          </div>
+        </div>
+      </div>
+      <div class="container-description">
+        <div class="asd">
+          <h2 class="feature-title">Unite a nuestro equipo</h2>
+          <p class="feature-description">
+            Podés unirte a nuestros departamentos
+            <a href="./contacto.html">enviándonos un mail</a>. También podés
+            colaborar con el sitio web a través de
+            <a href="https://github.com/francosbenitez/mepc">Github</a>.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section class="collaboration__content">
+      <div class="container-feature">
+        <div class="avatar">
+          <div class="avatar-image">
+            <img src="./assets/images/studying.svg" alt="Avatar MEPC" />
+          </div>
+        </div>
+      </div>
+      <div class="container-description">
+        <div class="asd">
+          <h2 class="feature-title">Unite a nuestros grupos de estudio</h2>
+          <p class="feature-description">
+            Contamos con
+            <a href="./que-estudiamos.html">nueve grupos de estudio</a> sobre
+            diferentes temas relacionados a la psicología científica. ¡Sumándote
+            a ellos podremos compartir y aprender en conjunto!
+          </p>
+        </div>
+      </div>
+    </section>
+  </section>
 </template>
 
 <style lang="scss">
-/* ---------- Features asd ----------  */
-
-.features-section-asd {
-  color: var(--color-text);
+.collaboration__content {
+  color: $color__text;
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
@@ -19,14 +87,14 @@
   border-radius: 50%;
 }
 
-.features-section-asd .container-description .asd .feature-title,
-.features-section-asd .container-description .asd .feature-description {
+.collaboration__content .container-description .asd .feature-title,
+.collaboration__content .container-description .asd .feature-description {
   margin: 1rem 0 1rem 0;
 }
 
 .container-description {
   flex-basis: 60%;
-  color: var(--color-text);
+  color: $color__text;
   display: flex;
   text-align: left;
 }
@@ -37,7 +105,7 @@
     text-align: center;
   }
 
-  .features-section-asd {
+  .collaboration__content {
     flex-direction: column;
     align-items: center;
   }
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <main class="home">
     <HomeHero />
+    <HomeFeatures2 />
   </main>
 </template>
 
 <script>
 import HomeHero from '../components/HomeHero'
+import HomeFeatures2 from '../components/HomeFeatures2'
 
 export default {
   name: 'IndexPage',
   components: {
     HomeHero,
+    HomeFeatures2,
   },
 }
 </script>
