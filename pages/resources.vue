6c7c0895174773c77a995f0ea88f9f1cce96a44e
francobenitez <franco.benitez@hogarthww.com>
2022-05-19 19:46:06 -0300

    style: finish basic scss of resources

--- pages/resources.vue
+++ pages/resources.vue
@@ -22,12 +22,12 @@
       />
       <div class="resources__content">
         <div
-          class="resource-card"
+          class="resources__card"
           v-for="item in content.resources"
           :key="item.title"
         >
           <a :href="item.url" target="_blank">
-            <div class="resource-card-texts">
+            <div class="resources__card-wrapper">
               <div class="resources__title">
                 {{ item.title }}
               </div>
@@ -68,72 +68,75 @@ export default {
   .study__title--primary {
     @include titles;
   }
-}
 
-.resources__content {
-  color: $color__text;
-  width: 100%;
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: space-evenly;
-  align-items: center;
-  margin: 0 auto;
-  max-width: 75vw;
-
-  .resource-card {
-    width: 250px;
-    height: auto;
-    box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
-    border: 1px solid rgba(0, 0, 0, 0.125);
-    border-radius: 20px;
-    overflow: hidden;
-    margin: 20px 10px;
-    transition: all 0.2s ease-in-out;
-
-    &:hover {
-      transform: scale(1.2);
-    }
-
-    hr {
-      margin-top: 1rem;
-      color: $color__text;
-    }
-
-    .resource-card-texts {
-      color: $color__text;
-      padding: 3rem 2rem;
-
-      .resources__title {
-        font-size: 1rem;
-        font-weight: 800;
-        text-align: center;
-      }
-
-      .resources__description {
-        font-size: 0.8rem;
-        margin-top: 1rem;
-        text-align: center;
-      }
+  .alert__note--right {
+    @media (max-width: $breakpoint--md) {
+      width: 90%;
     }
   }
-}
 
-.search-input {
-  width: 50%;
-  font-size: 16px;
-  padding: 12px 2rem 1rem 2rem;
-  margin-top: 1rem;
-  border: 1px solid $color__border;
-  background: #fff url('https://francosbenitez.github.io/mepc/img/search.svg')
-    1rem no-repeat;
-}
+  input {
+    width: 50%;
+    font-size: 16px;
+    padding: 12px 2rem 1rem 2rem;
+    margin-top: 1rem;
+    border: 1px solid $color__border;
+    background: #fff url('https://francosbenitez.github.io/mepc/img/search.svg')
+      1rem no-repeat;
 
-@media (max-width: 1300px) {
-  .alert__note--right,
-  .search-input {
-    width: 90%;
+    @media (max-width: $breakpoint--md) {
+      width: 90%;
+    }
+  }
+
+  .resources__content {
+    color: $color__text;
+    width: 100%;
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    align-items: center;
+    margin: 0 auto;
+    max-width: 75vw;
+
+    .resources__card {
+      width: 250px;
+      height: auto;
+      box-shadow: 0 2px 4px 0 rgb(0 0 0 / 20%);
+      border: 1px solid rgba(0, 0, 0, 0.125);
+      border-radius: 20px;
+      overflow: hidden;
+      margin: 20px 10px;
+      transition: all 0.2s ease-in-out;
+
+      &:hover {
+        transform: scale(1.2);
+      }
+
+      hr {
+        margin-top: 1rem;
+        color: $color__text;
+      }
+
+      .resources__card-wrapper {
+        color: $color__text;
+        padding: 3rem 2rem;
+
+        .resources__title {
+          font-size: 1rem;
+          font-weight: 800;
+          text-align: center;
+        }
+
+        .resources__description {
+          font-size: 0.8rem;
+          margin-top: 1rem;
+          text-align: center;
+        }
+      }
+    }
   }
 }
 </style>
