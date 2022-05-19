0db2563c944857e76b2f89aea78ef3628c7ee4f0
francobenitez <franco.benitez@hogarthww.com>
2022-05-18 23:28:15 -0300

    feat: add scss & html to documents view

--- pages/documents.vue
+++ pages/documents.vue
@@ -1,5 +1,133 @@
 <template>
-  <main></main>
+  <main>
+    <!-- Secondary navbar -->
+    <nav id="navbar">
+      <div class="secondary-nav">
+        <header>
+          <input
+            type="text"
+            id="search"
+            onkeyup="myFunction()"
+            placeholder="Buscar..."
+          />
+        </header>
+      </div>
+      <div class="ul-container" id="nav">
+        <ul>
+          <li class="hvr-sweep-to-top link active">
+            <a class="nav-link" href="#departamentos"
+              ><h1><i class="fas fa-book pr-1"></i> Departamentos</h1></a
+            >
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#delegados">Delegados</a>
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#diseno">Diseño</a>
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#divulgacion">Divulgación</a>
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#recursos-humanos">Recursos Humanos</a>
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#relaciones-publicas"
+              >Relaciones Públicas</a
+            >
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#revision">Revisión</a>
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#departamentos"
+              ><h1><i class="fas fa-book-reader pr-1"></i> Áreas</h1></a
+            >
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#revision">Facebook</a>
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#revision">Twitter</a>
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#revision">YouTube</a>
+          </li>
+          <li class="hvr-sweep-to-top link">
+            <a class="nav-link" href="#revision">Instagram</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Sections -->
+    <main id="main-doc">
+      <section class="main-section" id="departamentos">
+        <header>Departamentos</header>
+        <hr />
+        <p>
+          Los Departamentos son los ejes centrales de la organización de MEPC.
+        </p>
+      </section>
+      <section class="main-section" id="delegados">
+        <header>Departamento de Delegados</header>
+        <div class="img-container">
+          <img src="img/delegados.png" alt="Logo de Delegados MEPC" />
+        </div>
+        <hr />
+        <hr />
+        <h3>¿Qué es?</h3>
+        <p>
+          El grupo de Delegades reúne a quienes cumplen dicho rol en cada uno de
+          los grupos de estudio. Estará EXCLUSIVAMENTE compuesto por quienes
+          sean los voceros de cada grupo, siendo éste un criterio excluyente. El
+          grupo va a tener una composición variable, debido a que el puesto será
+          rotativo.
+        </p>
+        <h3>¿Cuáles son sus funciones?</h3>
+        <ul class="properties">
+          <li>
+            Función general: ser un espacio para hablar, debatir y organizar
+            cuestiones exclusivamente ligadas al funcionamiento de los grupos de
+            estudio.
+          </li>
+        </ul>
+        <ul class="properties">
+          <li>
+            Funciones específicas:
+            <ul>
+              <li>Resolver cuestiones administrativas de cada grupo.</li>
+              <li>Compartir información relevante sobre cada grupo.</li>
+              <li>
+                Organizar la generación de contenido en cada grupo (se realiza
+                en conjunto con todos los miembros activos pero las delegadas
+                serían las que le den el toque final para organizar toda esa
+                información de manejar formal).
+              </li>
+              <li>
+                Delegar las reuniones quincenales a los miembros activos* para
+                poder hacerse cargo de los links de acceso a las reuniones.
+              </li>
+              <li>
+                Estipular pautas de funcionamiento común para todos los grupos
+                sobre: cantidad de delegades, difusión de links de WhatsApp,
+                grabación de reuniones, moderación de mensajes offtopic,
+                procedimientos para elección de nuevos delegades y tiempo en
+                ejercicio de funciones, etc
+              </li>
+              <li>
+                Programar actividades en conjunto con otros grupos (de estudio u
+                otros departamentos) -para eso habrá delegado de cada
+                departamento-.
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <h3>¿Cómo formar parte?</h3>
+        <p>Enviando un mail a mepc.org@gmail.com.</p>
+      </section>
+    </main>
+  </main>
 </template>
 
 <style lang="scss">
@@ -64,8 +192,10 @@ a:active {
   font-weight: 800;
 }
 
-#navbar {
-  @media (min-width: 850px) {
+@media (min-width: 850px) {
+  /* Secondary navbar */
+
+  #navbar {
     width: 20rem;
     height: 100vh;
     position: fixed;
@@ -77,25 +207,27 @@ a:active {
     justify-content: center;
     padding: 5rem 0 0 0;
     border-right: 1px solid var(--border-color);
+  }
 
-    .secondary-nav {
-      width: 100%;
-      font-size: 16px;
-      padding: 12px 20px 12px;
-      border-bottom: 1px solid var(--border-color);
-    }
+  #navbar .secondary-nav {
+    width: 100%;
+    font-size: 16px;
+    padding: 12px 20px 12px;
+    border-bottom: 1px solid var(--border-color);
+  }
 
-    .ul-container {
-      width: 100%;
-      height: auto;
-      overflow: auto;
-      border: none;
-      margin: 0 0 0 0;
-    }
+  #navbar .ul-container {
+    width: 100%;
+    height: auto;
+    overflow: auto;
+    border: none;
+    margin: 0 0 0 0;
+  }
 
-    #main-doc {
-      margin: 0 0 0 21rem;
-    }
+  /* Sections */
+
+  #main-doc {
+    margin: 0 0 0 21rem;
   }
 }
 
