e237cdbc16e91a287c9176594492f8e3b81ae1d2
francobenitez <franco.benitez@hogarthww.com>
2022-05-19 11:42:07 -0300

    style: add container to documents

--- pages/documents.vue
+++ pages/documents.vue
@@ -1,41 +1,45 @@
 <template>
   <main>
-    <nav id="navbar">
-      <div class="secondary-nav">
-        <header>
-          <input type="text" id="search" placeholder="Buscar..." />
-        </header>
-      </div>
-      <div class="ul-container" id="nav">
-        <ul>
-          <li class="hvr-sweep-to-top link active">
-            <a class="nav-link" href="#departamentos"><h1>Departamentos</h1></a>
-          </li>
-          <li
-            class="hvr-sweep-to-top link"
-            v-for="item in content.documents.departments"
-            :key="item.name"
-          >
-            <a class="nav-link" :href="item.url">{{ item.name }}</a>
-          </li>
-          <li class="hvr-sweep-to-top link">
-            <a class="nav-link" href="#departamentos"><h1>Áreas</h1></a>
-          </li>
-          <li
-            class="hvr-sweep-to-top link"
-            v-for="item in content.documents.areas"
-            :key="item.name"
-          >
-            <a class="nav-link" :href="item.url">{{ item.name }}</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <main id="main-doc">
-      <section class="main-section" id="departamentos">
-        <nuxt-content :document="markdown" />
-      </section>
-    </main>
+    <section id="documents">
+      <nav id="navbar">
+        <div class="secondary-nav">
+          <header>
+            <input type="text" id="search" placeholder="Buscar..." />
+          </header>
+        </div>
+        <div class="ul-container" id="nav">
+          <ul>
+            <li class="hvr-sweep-to-top link active">
+              <a class="nav-link" href="#departamentos"
+                ><h1>Departamentos</h1></a
+              >
+            </li>
+            <li
+              class="hvr-sweep-to-top link"
+              v-for="item in content.documents.departments"
+              :key="item.name"
+            >
+              <a class="nav-link" :href="item.url">{{ item.name }}</a>
+            </li>
+            <li class="hvr-sweep-to-top link">
+              <a class="nav-link" href="#departamentos"><h1>Áreas</h1></a>
+            </li>
+            <li
+              class="hvr-sweep-to-top link"
+              v-for="item in content.documents.areas"
+              :key="item.name"
+            >
+              <a class="nav-link" :href="item.url">{{ item.name }}</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <main id="main-doc">
+        <section class="main-section" id="departamentos">
+          <nuxt-content :document="markdown" />
+        </section>
+      </main>
+    </section>
   </main>
 </template>
 
@@ -58,143 +62,152 @@ export default {
 </script>
 
 <style lang="scss">
-#navbar {
-  width: 100%;
-  padding: 5rem 0.2rem 0 0.2rem;
-  margin: 0 0 1rem 0;
-  text-align: center;
+#documents {
+  max-width: 1152px;
+  position: relative;
+  width: 90%;
+  margin: 0 auto;
+  padding-top: 5rem;
+  padding-bottom: 5rem;
 
-  .secondary-nav {
-    #search {
-      width: 100%;
-      font-size: 16px;
-      padding: 12px 20px 12px 40px;
-      border: 1px solid $color__border;
-      margin-bottom: 12px;
-      background: #fff
-        url('https://francosbenitez.github.io/mepc/img/search.svg') 1rem
-        no-repeat;
-    }
-  }
-
-  .ul-container {
-    margin: 1rem 0 0 0;
-    width: 100%;
-    height: 10rem;
-    overflow-y: scroll;
-    border: 1px solid $color__border;
-
-    li {
-      list-style: none;
-
-      a {
-        display: block;
-        text-align: left;
-        font-size: 1rem;
-        padding: 0.5rem;
-        text-decoration: none;
-        color: $color__text;
-
-        &:link,
-        &:visited,
-        &:active,
-        &:hover {
-          color: $color__primary;
-        }
-      }
-    }
-
-    .active a {
-      color: $color__primary;
-      font-weight: 800;
-      border-left: solid 4px $color__primary;
-      margin-left: 0.2rem;
-      color: $color__primary;
-      font-weight: 800;
-
-      h1 {
-        /* color: $color__primary;
-        font-weight: 800; */
-      }
-    }
-  }
-}
-
-@media (min-width: 850px) {
   #navbar {
-    width: 20rem;
-    height: 100vh;
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    display: flex;
-    flex-flow: column nowrap;
-    align-items: center;
-    justify-content: center;
-    padding: 5rem 0 0 0;
-    border-right: 1px solid $color__border;
+    width: 100%;
+    padding: 5rem 0.2rem 0 0.2rem;
+    margin: 0 0 1rem 0;
+    text-align: center;
 
     .secondary-nav {
-      width: 100%;
-      font-size: 16px;
-      padding: 12px 20px 12px;
-      border-bottom: 1px solid $color__border;
+      #search {
+        width: 100%;
+        font-size: 16px;
+        padding: 12px 20px 12px 40px;
+        border: 1px solid $color__border;
+        margin-bottom: 12px;
+        background: #fff
+          url('https://francosbenitez.github.io/mepc/img/search.svg') 1rem
+          no-repeat;
+      }
     }
 
     .ul-container {
+      margin: 1rem 0 0 0;
       width: 100%;
-      height: auto;
-      overflow: auto;
-      border: none;
-      margin: 0 0 0 0;
+      height: 10rem;
+      overflow-y: scroll;
+      border: 1px solid $color__border;
+
+      li {
+        list-style: none;
+
+        a {
+          display: block;
+          text-align: left;
+          font-size: 1rem;
+          padding: 0.5rem;
+          text-decoration: none;
+          color: $color__text;
+
+          &:link,
+          &:visited,
+          &:active,
+          &:hover {
+            color: $color__primary;
+          }
+        }
+      }
+
+      .active a {
+        color: $color__primary;
+        font-weight: 800;
+        border-left: solid 4px $color__primary;
+        margin-left: 0.2rem;
+        color: $color__primary;
+        font-weight: 800;
+
+        h1 {
+          /* color: $color__primary;
+        font-weight: 800; */
+        }
+      }
+    }
+  }
+
+  @media (min-width: 850px) {
+    #navbar {
+      width: 20rem;
+      height: 100vh;
+      position: fixed;
+      top: 0;
+      bottom: 0;
+      display: flex;
+      flex-flow: column nowrap;
+      align-items: center;
+      justify-content: center;
+      padding: 5rem 0 0 0;
+      border-right: 1px solid $color__border;
+
+      .secondary-nav {
+        width: 100%;
+        font-size: 16px;
+        padding: 12px 20px 12px;
+        border-bottom: 1px solid $color__border;
+      }
+
+      .ul-container {
+        width: 100%;
+        height: auto;
+        overflow: auto;
+        border: none;
+        margin: 0 0 0 0;
+      }
+    }
+
+    #main-doc {
+      margin: 0 0 0 21rem;
     }
   }
 
   #main-doc {
-    margin: 0 0 0 21rem;
-  }
-}
-
-#main-doc {
-  padding: 0.5rem;
-
-  header {
-    font-size: 1.5rem;
-    font-weight: 600;
-    margin: 0.5rem 0 0.5rem 0;
-  }
-
-  .main-section {
-    h3 {
-      margin: 1rem 0 1rem 0;
-    }
-
-    .properties {
-      li {
-        margin: 0 0 0 3rem;
-        font-size: 1.1rem;
-      }
-    }
+    padding: 0.5rem;
 
     header {
-      margin: 5rem 0 1rem 0;
+      font-size: 1.5rem;
+      font-weight: 600;
+      margin: 0.5rem 0 0.5rem 0;
     }
 
-    .img-container {
-      width: 100%;
-      max-width: 35rem;
-      margin: 0 auto;
-
-      img {
-        display: block;
-        width: 100%;
-        height: auto;
+    .main-section {
+      h3 {
         margin: 1rem 0 1rem 0;
       }
-    }
 
-    hr {
-      margin: 1rem 0 1rem 0;
+      .properties {
+        li {
+          margin: 0 0 0 3rem;
+          font-size: 1.1rem;
+        }
+      }
+
+      header {
+        margin: 5rem 0 1rem 0;
+      }
+
+      .img-container {
+        width: 100%;
+        max-width: 35rem;
+        margin: 0 auto;
+
+        img {
+          display: block;
+          width: 100%;
+          height: auto;
+          margin: 1rem 0 1rem 0;
+        }
+      }
+
+      hr {
+        margin: 1rem 0 1rem 0;
+      }
     }
   }
 }
