433a220c24c8bbd660cde5153c07faf84aa608b5
francobenitez <franco.benitez@hogarthww.com>
2022-07-29 13:32:07 -0300

    feat: add homeabout component

---
+++ components/HomeAbout/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+const HomeAbout = () => {
+  return (
+    <section id={styles.about}>
+      <div className={styles.about__avatar}>
+        <div>
+          <div className={styles.about__image}>
+            <Image
+              width="100"
+              height="100"
+              src="/images/avatar.png"
+              alt="Avatar MEPC"
+            />
+          </div>
+          <div className={styles.about__social}>
+            <h2>MEPC</h2>
+            <h3>Movimiento Estudiantil por la Psicología Científica</h3>
+            <ul className={styles.about__items}>
+              {/* <template v-for="item in content.social_networks">
+              <a :href="item.url" :key="item.url" className="about__items-networks">
+                <component
+                  :is="item.component"
+                  :key="item.component"
+                ></component>
+              </a>
+            </template> */}
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className={styles.about__video}>
+        <div>
+          <div className={styles["about__iframe-wrapper"]}>
+            <iframe
+              width="800"
+              height="500"
+              src="https://www.youtube.com/embed/ieSG5JVLOow"
+            ></iframe>
+          </div>
+          <div className={styles.about__description}>
+            <hr />
+            <p>
+              Somos el Movimiento Estudiantil por la Psicología Científica
+              (MEPC), un grupo de estudiantes y graduades autoconvocades de
+              diferentes puntos del país que compartimos un mismo ideal: el
+              desarrollo de una formación y una práctica profesional basadas en
+              fundamentos científicos. Nacimos en la Facultad de Psicología de
+              la Universidad de Buenos Aires (UBA), y ya sumamos casi 3000
+              personas de diferentes casas de estudio que creemos que la ciencia
+              es el camino que debemos seguir en psicología para mejorar nuestra
+              formación.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeAbout;
--- components/HomeHero/index.tsx
+++ components/HomeHero/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-
 import styles from "./styles.module.scss";
 
 const HomeHero = () => {
