60f87700e73342cadae514987119e34d9b69df8d
francobenitez <franco.benitez@hogarthww.com>
2022-08-03 23:52:04 -0300

    style: modify navbar

--- components/global/TheNavbar/index.tsx
+++ components/global/TheNavbar/index.tsx
@@ -7,14 +7,14 @@ const TheNavbar = () => {
   const [clicked, setClicked] = useState<boolean>(false);
 
   return (
-    <header className={styles.header}>
-      <div className={styles.header__content}>
-        <nav className={styles.nav}>
-          <Link href="/" className={styles.nav__logo}>
+    <header className={styles["header"]}>
+      <div className={styles["header-content"]}>
+        <nav className={styles["nav"]}>
+          <Link href="/" className={styles["nav-logo"]}>
             <Image width="40" height="40" src="/icons/logo.svg" alt="" />
           </Link>
           <ul
-            className={`${styles.nav__list} ${
+            className={`${styles["nav-list"]} ${
               clicked ? styles["nav-active"] : ""
             }`}
           >
@@ -29,12 +29,14 @@ const TheNavbar = () => {
             </li>
           </ul>
           <div
-            className={`${styles.nav__btn} ${clicked ? styles.toggle : ""}`}
+            className={`${styles["nav-btn"]} ${
+              clicked ? styles["toggle"] : ""
+            }`}
             onClick={() => setClicked(!clicked)}
           >
-            <div className={styles.line__1}></div>
-            <div className={styles.line__2}></div>
-            <div className={styles.line__3}></div>
+            <div className={styles["line-1"]}></div>
+            <div className={styles["line-2"]}></div>
+            <div className={styles["line-3"]}></div>
           </div>
         </nav>
       </div>
