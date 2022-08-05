42043086b8260628868ed39c6cdd7a578644e4ac
francobenitez <franco.benitez@hogarthww.com>
2022-08-05 00:18:12 -0300

    feat: add montserrat regular font as a server font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,12 +7,7 @@ class Document extends NextDocument<Props> {
   render() {
     return (
       <Html>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap"
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
