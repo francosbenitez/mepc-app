ae76340d2b36237c25aa5fa52e2fdb147e607ce2
francobenitez <franco.benitez@hogarthww.com>
2022-08-05 01:11:11 -0300

    style: improve footer adding icons of social networks

--- components/global/TheFooter.tsx
+++ components/global/TheFooter.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import Email from "../../public/icons/email.svg";
+import Instagram from "../../public/icons/instagram.svg";
+import Facebook from "../../public/icons/facebook.svg";
 
 const TheFooter = () => {
   return (
     <footer className="bottom-0 w-full mx-auto p-5 sm:px-0 md:py-8 footer">
       <div className="container relative text-center m-auto">
-        <div className="relative text-center m-auto flex">
-          <div className="w-1/2 flex">
-            <span className="text-left text-sm md:text-md">Nuestras redes</span>
+        <div className="relative text-center m-auto flex justify-end">
+          <div className="flex">
+            <Email className="mx-3" />
+            <Instagram className="mx-3" />
+            <Facebook className="mx-3" />
           </div>
         </div>
       </div>
