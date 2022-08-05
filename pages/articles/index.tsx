4a74c08f544b67a1b09a26b1cd5e63536996ec69
francobenitez <franco.benitez@hogarthww.com>
2022-08-05 10:19:39 -0300

    feat: add article card

--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,7 +1,45 @@
 import React from "react";
+import Image from "next/image";
 
 const Articles = () => {
-  return <main className="wrapper">Articles</main>;
+  return (
+    <main className="wrapper">
+      <div className="container mx-auto pt-20">
+        <h1 className="text-center text-3xl">Nuestros artículos</h1>
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <Image
+              width="100"
+              height="100"
+              layout="responsive"
+              objectFit="contain"
+              src="/images/ocean.jpeg"
+              alt=""
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Voluptatibus quia, nulla! Maiores et perferendis eaque,
+                exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Articles;
