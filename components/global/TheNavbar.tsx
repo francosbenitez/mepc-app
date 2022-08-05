e877444b0f537c4a888f9c1c57686c08c2658579
francobenitez <franco.benitez@hogarthww.com>
2022-08-05 00:32:52 -0300

    style: improve nabar

--- components/global/TheNavbar.tsx
+++ components/global/TheNavbar.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const TheNavbar = () => {
-  return <>Hello World</>;
+  return (
+    <header className="w-full mx-auto p-5 sm:px-0 navbar">
+      <div className="container m-auto">
+        <ul className="flex justify-end">
+          <li className="p-2 self-center">Artículos</li>
+          <li className="p-2 self-center">
+            <button className="rounded btn bg-primary text-white p-2 border-white">
+              Publicá
+            </button>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default TheNavbar;
