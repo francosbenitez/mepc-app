e1efdc39164b6491b87f1c1b4f572b94f6731fbe
francobenitez <franco.benitez@hogarthww.com>
2022-08-05 01:30:21 -0300

    style: improve theabout

--- components/home/TheAbout.tsx
+++ components/home/TheAbout.tsx
@@ -4,16 +4,19 @@ const TheAbout = () => {
   return (
     <div className="container m-auto">
       <div className="w-1/2 m-auto">
-        <p className="text-center py-16">
-          Somos un grupo de estudiantes y graduades autoconvocades, provenientes
-          de diferentes puntos del país, que compartimos un mismo ideal: el
-          desarrollo de una formación y una práctica profesional basadas en
-          fundamentos científicos. Nacimos en la Facultad de Psicología de la
-          Universidad de Buenos Aires (UBA), y ya sumamos alrededor de 3000
-          personas que creemos que la ciencia es el camino que debemos seguir en
-          psicología para mejorar nuestra formación.
-        </p>
-        <div className="">
+        <div className="text-center py-16">
+          <p className="mb-6 text-2xl">¿Quiénes somos?</p>
+          <p>
+            Somos un grupo de estudiantes y graduades autoconvocades,
+            provenientes de diferentes puntos del país, que compartimos un mismo
+            ideal: el desarrollo de una formación y una práctica profesional
+            basadas en fundamentos científicos. Nacimos en la Facultad de
+            Psicología de la Universidad de Buenos Aires (UBA), y ya sumamos
+            alrededor de 3000 personas que creemos que la ciencia es el camino
+            que debemos seguir en psicología para mejorar nuestra formación.
+          </p>
+        </div>
+        <div>
           <div className="iframe-wrapper">
             <iframe
               width="500"
