69fd115a9c2e7329cf651440e20c66911ea466ac
francobenitez <franco.benitez@hogarthww.com>
2022-08-09 00:51:47 -0300

    feat: implmeent TheCard component

--- components/global/TheCard.tsx
+++ components/global/TheCard.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+// import Image from "next/image";
 
-const TheCard = () => {
-  return <div>TheCard</div>;
+const TheCard = ({ article }: { article: Record<string, any> }) => {
+  return (
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      {/* <Image
+    width="100"
+    height="100"
+    layout="responsive"
+    objectFit="contain"
+    src="/images/ocean.jpeg"
+    alt=""
+  /> */}
+      <div className="px-6 py-4">
+        <div className="font-bold text-xl mb-2">{article.title}</div>
+        <p className="text-gray-700 text-base">{article.content}</p>
+      </div>
+      {article.tags.length > 0 && (
+        <>
+          {article.tags.map((item: Record<string, any>) => (
+            <div className="px-6 pt-4 pb-2" key={item.tag.id}>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #{item.tag.name}
+              </span>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default TheCard;
--- pages/articles/index.tsx
+++ pages/articles/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import Image from "next/image";
 import Head from "next/head";
 import { GetServerSideProps } from "next";
 import ArticlesService from "../../services/ArticlesService";
 import MainLayout from "../../layouts/main";
+import TheCard from "../../components/global/TheCard";
 
 const Articles = ({ articles }: { articles: any }) => {
   console.log("articles", articles);
@@ -17,36 +17,8 @@ const Articles = ({ articles }: { articles: any }) => {
           <h1 className="text-center text-3xl font-mb">Nuestros artículos</h1>
           {/* Add a filter here! */}
           <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-            {/* Card */}
             {articles.data.map((item: Record<string, any>) => (
-              <div
-                className="max-w-sm rounded overflow-hidden shadow-lg"
-                key={item.id}
-              >
-                {/* <Image
-                  width="100"
-                  height="100"
-                  layout="responsive"
-                  objectFit="contain"
-                  src="/images/ocean.jpeg"
-                  alt=""
-                /> */}
-                <div className="px-6 py-4">
-                  <div className="font-bold text-xl mb-2">{item.title}</div>
-                  <p className="text-gray-700 text-base">{item.content}</p>
-                </div>
-                {item.tags.length > 0 && (
-                  <>
-                    {item.tags.map((item: any) => (
-                      <div className="px-6 pt-4 pb-2" key={item.tag.id}>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #{item.tag.name}
-                        </span>
-                      </div>
-                    ))}
-                  </>
-                )}
-              </div>
+              <TheCard article={item} key={item.id} />
             ))}
           </div>
         </div>
