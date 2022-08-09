76dc01d636dd911fb9791a668a1e8bbf0ecdad7e
francobenitez <franco.benitez@hogarthww.com>
2022-08-09 19:39:46 -0300

    feat: add head to dashboard

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import DashboardLayout from "../../layouts/dashboard";
+import Head from "next/head";
 
 const Dashboard = () => {
   return (
     <>
+      <Head>
+        <title>Dashboard - MEPC</title>
+      </Head>
       <main className="wrapper">
         <div>Dashboard</div>
       </main>
