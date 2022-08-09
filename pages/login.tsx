99fa8392b45fbad3578727690442836c15805085
francobenitez <franco.benitez@hogarthww.com>
2022-08-08 22:29:12 -0300

    style: login changes

--- pages/login.tsx
+++ pages/login.tsx
@@ -59,30 +59,28 @@ const Login = () => {
 
               <div className="flex items-center justify-between">
                 <div className="flex items-center">
-                  <input
+                  {/* <input
                     id="remember-me"
                     name="remember-me"
                     type="checkbox"
                     className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                  />
+                  /> */}
                   <label
                     htmlFor="remember-me"
                     className="ml-2 block text-sm text-gray-900"
                   >
-                    {" "}
-                    Recuérdame{" "}
+                    ¿No tenés una cuenta?
+                    <a
+                      href="#"
+                      className="font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                      {" "}
+                      Registrate{" "}
+                    </a>
                   </label>
                 </div>
 
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    {" "}
-                    Registrate{" "}
-                  </a>
-                </div>
+                <div className="text-sm"></div>
               </div>
 
               <div>
