4c632eab94c4e5e0262ce6be6ba0199ef988ce73
francobenitez <franco.benitez@hogarthww.com>
2022-08-09 00:57:50 -0300

    feat: implement articles in the home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import TheAbout from "../components/home/TheAbout";
 import MainLayout from "../layouts/main";
+import { GetServerSideProps } from "next";
+import ArticlesService from "../services/ArticlesService";
+import TheCard from "../components/global/TheCard";
 
-const Home = () => {
+const Home = ({ articles }: { articles: any }) => {
   return (
     <>
       <Head>
@@ -16,7 +19,12 @@ const Home = () => {
         <TheAbout />
         <section className="container m-auto">
           <div className="py-16">
-            <p className="text-2xl font-mb">Artículos recientes</p>
+            <p className="text-2xl font-mb my-16">Artículos recientes</p>
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+              {articles.data.map((item: Record<string, any>) => (
+                <TheCard article={item} key={item.id} />
+              ))}
+            </div>
           </div>
         </section>
       </main>
@@ -24,6 +32,14 @@ const Home = () => {
   );
 };
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  const articles = (await ArticlesService.index(1)).data;
+  return {
+    props: {
+      articles,
+    },
+  };
+};
 Home.layout = MainLayout;
 
 export default Home;
