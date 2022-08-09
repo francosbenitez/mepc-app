476e197c14dd3149631d353e6298db066ea33b61
francobenitez <franco.benitez@hogarthww.com>
2022-08-09 19:15:36 -0300

    style: improve footer

--- components/global/TheFooter.tsx
+++ components/global/TheFooter.tsx
@@ -7,7 +7,9 @@ const TheFooter = () => {
   return (
     <footer className="bottom-0 w-full mx-auto p-5 sm:px-0 md:py-8 footer">
       <div className="container text-center md:flex m-auto">
-        <div className="block md:flex md:flex-column md:w-1/2">© MEPC 2022</div>
+        <div className="block md:flex md:flex-column md:w-1/2 font-mm">
+          MEPC © 2022
+        </div>
         <div className="block md:flex md:flex-column md:w-1/2 md:justify-end">
           <div className="flex justify-center mt-3 md:mt-0">
             <Email className="mx-3 fill-primary" />
