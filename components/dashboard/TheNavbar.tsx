e7df73b86f8a2d28ff9aa1ff4443a5bab8e49afb
francobenitez <franco.benitez@hogarthww.com>
2022-08-11 01:14:10 -0300

    feat: add 'role' state to the redux store & add 'salir' btn to the dashboard

--- components/dashboard/TheNavbar.tsx
+++ components/dashboard/TheNavbar.tsx
@@ -4,15 +4,13 @@ const TheNavbar = (props: any) => {
   return (
     <div className="header-section" id="sticky">
       <nav className="navbar navbar-expand-lg navbar-light">
-        <div className="flex flex-nowrap">
+        <div className="flex flex-nowrap w-full">
           <div className="nav-title">
             <Link href="/dashboard">
               <a className="z-20 text-primary">MEPC Dashboard</a>
             </Link>
           </div>
-
           <div onClick={() => props.onToggleClick()}>
-            {/* <i className="fa fa-bars" aria-hidden="true"></i> */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -22,11 +20,13 @@ const TheNavbar = (props: any) => {
               <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" />
             </svg>
           </div>
-
           <div
             className="collapse navbar-collapse"
             id="navbarSupportedContent"
           ></div>
+          <button className="border border-primary p-2 rounded ml-auto">
+            Salir
+          </button>
         </div>
       </nav>
     </div>
