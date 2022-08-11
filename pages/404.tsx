d9242bb735266ff1faa8bf3bc850ef54bf74fc5d
francobenitez <franco.benitez@hogarthww.com>
2022-08-10 23:49:50 -0300

    fix: issue about 500/404 pages

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import BlankLayout from "../layouts/blank";
+
+const Error404 = () => {
+  return <div>Error404</div>;
+};
+
+Error404.layout = BlankLayout;
+
+export default Error404;
---
+++ pages/500.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import BlankLayout from "../layouts/blank";
+
+const Error500 = () => {
+  return <div>Error500</div>;
+};
+
+Error500.layout = BlankLayout;
+
+export default Error500;
