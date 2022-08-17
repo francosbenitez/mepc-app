b925e695eb04828bf6b2e1a009accb739123f453
francobenitez <franco.benitez@hogarthww.com>
2022-08-16 23:14:59 -0300

    feat: add more footer icons

--- components/global/TheFooter.tsx
+++ components/global/TheFooter.tsx
@@ -1,12 +1,25 @@
-import React from "react";
-import Email from "../../public/icons/email.svg";
-import Instagram from "../../public/icons/instagram.svg";
-import Facebook from "../../public/icons/facebook.svg";
-import Twitter from "../../public/icons/twitter.svg";
-import WhatsApp from "../../public/icons/whatsapp.svg";
-import YouTube from "../../public/icons/youtube.svg";
+import React, { ReactNode } from "react";
+import Email from "@/public/icons/email.svg";
+import Instagram from "@/public/icons/instagram.svg";
+import Twitter from "@/public/icons/twitter.svg";
+import WhatsApp from "@/public/icons/whatsapp.svg";
+import YouTube from "@/public/icons/youtube.svg";
+import Discord from "@/public/icons/discord.svg";
+import FacebookPage from "@/public/icons/facebook_page.svg";
+import FacebookGroup from "@/public/icons/facebook_group.svg";
 
 const TheFooter = () => {
+  const icons: ReactNode[] = [
+    Email,
+    Instagram,
+    FacebookPage,
+    FacebookGroup,
+    Discord,
+    Twitter,
+    WhatsApp,
+    YouTube,
+  ];
+
   return (
     <footer className="bottom-0 w-full mx-auto p-5 sm:px-0 md:py-8 footer">
       <div className="container text-center md:flex m-auto w-11/12 mx-auto py-10">
@@ -15,12 +28,9 @@ const TheFooter = () => {
         </div>
         <div className="block md:flex md:flex-column md:w-1/2 md:justify-end">
           <div className="flex justify-center mt-3 md:mt-0">
-            <Email className="mx-3 fill-primary" />
-            <Instagram className="mx-3 fill-primary" />
-            <Facebook className="mx-3 fill-primary" />
-            <Twitter className="mx-3 fill-primary" />
-            <WhatsApp className="mx-3 fill-primary" />
-            <YouTube className="mx-3 fill-primary" />
+            {icons.map((Icon: any) => {
+              return <Icon key={Icon} className="mx-3 fill-primary" />;
+            })}
           </div>
         </div>
       </div>
